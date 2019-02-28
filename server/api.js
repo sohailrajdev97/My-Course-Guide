f38a7e4d716128137dd3676489f1649cedddf7e8
iamkroot <kroot.patel@gmail.com>
2019-02-28 23:06:22 +0530

    api: Add POST for /departments

--- server/api.js
+++ server/api.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 const auth = require("./auth");
+
+const { Department } = require("./db/models/department");
+const { Professor } = require("./db/models/professor");
 
 router.all("/", (req, res, next) => {
   console.log(`${req.method} for ${req.url}`);
@@ -9,5 +13,33 @@ router.all("/", (req, res, next) => {
 });
 
 router.use("/auth", auth);
+
+router.use("/", bodyParser.json());
+
+router.post("/departments", async (req, res) => {
+  const body = _.pick(req.body, ["name", "campus", "hod"]);
+  try {
+    body.hod = await Professor.findOne({ email: body.hod });
+  } catch (err) {
+    delete body.hod;
+  } // don't care if hod doesn't exist yet
+
+  let dept = null;
+  // get department from parameters
+  try {
+    dept = await Department.findOne({ name: body.name, campus: body.campus });
+  } catch (err) {}
+  // create the department if it doesn't exist
+  if (!dept) {
+    dept = new Department(body);
+  }
+
+  try {
+    await dept.save();
+    res.send(200);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 module.exports = router;
