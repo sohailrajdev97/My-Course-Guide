6a6299d27af7762b2bda3d8ab2f1f89e2d789901
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-03-01 21:56:33 +0530

    client: add custom axios methods

---
+++ src/Routes/utils/axiosClient.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+const qs = require("qs");
+
+let axiosDELETE = url => {
+  let options = {
+    method: "DELETE",
+    headers: {
+      "x-access-token": sessionStorage.getItem("token")
+        ? sessionStorage.getItem("token")
+        : null
+    },
+    url: url
+  };
+  return axios(options);
+};
+
+let axiosGET = (url, resType) => {
+  let options = {
+    method: "GET",
+    headers: {
+      "x-access-token": sessionStorage.getItem("token")
+        ? sessionStorage.getItem("token")
+        : null
+    },
+    responseType: resType,
+    url: url
+  };
+  return axios(options);
+};
+
+let axiosPOST = (url, data) => {
+  let options = {
+    method: "POST",
+    headers: {
+      "x-access-token": sessionStorage.getItem("token")
+        ? sessionStorage.getItem("token")
+        : null
+    },
+    data: qs.stringify(data),
+    url: url
+  };
+  return axios(options);
+};
+
+let axiosPUT = (url, data) => {
+  let options = {
+    method: "PUT",
+    headers: {
+      "x-access-token": sessionStorage.getItem("token")
+        ? sessionStorage.getItem("token")
+        : null
+    },
+    data: qs.stringify(data),
+    url: url
+  };
+  return axios(options);
+};
+
+module.exports = {
+  axiosDELETE,
+  axiosGET,
+  axiosPOST,
+  axiosPUT
+};
