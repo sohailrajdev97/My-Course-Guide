fc7f4642e0673ea5d0a0330927d24e31630947c3
iamkroot <kroot.patel@gmail.com>
2019-03-04 10:33:25 +0530

    client: Add Handouts Pane in Course Overview

--- client/components/Course.jsx
+++ client/components/Course.jsx
@@ -4,6 +4,8 @@ import { axiosGET } from "../utils/axiosClient";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
 
 class Course extends Component {
   constructor(props) {
@@ -35,21 +37,49 @@ class Course extends Component {
     if (!this.state.course) {
       return null;
     }
+    let getCards = () => {
+      return this.state.course.history.map(item => (
+        <Card key={item.year * 10 + item.sem}>
+          <Card.Body
+            style={{
+              minWidth: "7rem",
+              minHeight: "10rem"
+            }}
+          >
+            <Card.Title>
+              <h5>{item.year}</h5>
+              <h6>Sem {item.semester}</h6>
+            </Card.Title>
+            <Card.Footer>{item.professor.name}</Card.Footer>
+          </Card.Body>
+        </Card>
+      ));
+    };
     return (
-      <Container>
-        <br />
-        <Row>
-          <Col lg={8}>
-            <h6>{this.state.course.id}</h6>
-            <h3>{this.state.course.name}</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={8}>
-            <h5>{this.state.course.history[0].professor.name}</h5>
-          </Col>
-        </Row>
-      </Container>
+      <div className="container-fluid">
+        <Container>
+          <br />
+          <Row>
+            <Col lg={8}>
+              <h6>{this.state.course.id}</h6>
+              <h3>{this.state.course.name}</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={8}>
+              <h5>{this.state.course.history[0].professor.name}</h5>
+            </Col>
+          </Row>
+          <br />
+          <br />
+          <p>Download Previous Year Handouts</p>
+          <Row style={{ overflowX: "auto" }}>
+            <CardDeck className="d-flex flex-row flex-nowrap">
+              {getCards()}
+            </CardDeck>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
