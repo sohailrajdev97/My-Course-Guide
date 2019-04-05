61cd1279b24f49e321f7146aec555a8d0a9ffad2
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-04-05 20:01:50 +0530

    db: shift to mongoose timestamps

--- server/db/models/reply.js
+++ server/db/models/reply.js
@@ -1,33 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const replySchema = new Schema({
-  record: {
-    _id: false,
-    type: Schema.Types.ObjectId,
-    ref: "Record",
-    index: true,
-    required: true
+const replySchema = new Schema(
+  {
+    record: {
+      _id: false,
+      type: Schema.Types.ObjectId,
+      ref: "Record",
+      index: true,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    replierType: {
+      type: String, // Either "Professor" or "Student"
+      required: true
+    },
+    by: {
+      _id: false,
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "replierType"
+    }
   },
-  content: {
-    type: String,
-    required: true
-  },
-  replierType: {
-    type: String, // Either "Professor" or "Student"
-    required: true
-  },
-  by: {
-    _id: false,
-    type: Schema.Types.ObjectId,
-    required: true,
-    refPath: "replierType"
-  },
-  time: {
-    type: Date,
-    default: new Date()
+  {
+    timestamps: true
   }
-});
+);
 
 replySchema.pre("find", function(next) {
   this.populate({ path: "by", select: { _id: 0, name: 1, id: 1 } });
