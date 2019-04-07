ee7a7e563cde69a13c6d5374fe6ec8297205db4b
iamkroot <kroot.patel@gmail.com>
2019-04-07 11:07:44 +0530

    questions: Add mark as helpful buttons

--- src/components/QuestionSection.jsx
+++ src/components/QuestionSection.jsx
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import TimeAgo from "react-timeago";
 import SeeAll from "./SeeAll";
+import { axiosPOST } from "../utils/axiosClient";
+import { getDecodedToken } from "../utils/jwt";
 
 class QuestionSection extends Component {
   constructor(props) {
     super(props);
-    this.state = { questions: [] };
+    this.state = { questions: [], votes: { Record: {}, Reply: {} } };
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ questions: [...nextProps.questions] });
+    this.setState({
+      questions: [...nextProps.questions],
+      votes: { ...nextProps.votes }
+    });
   }
 
   render() {
+    let isStudent = getDecodedToken().role === "student";
     let genAnswers = question => {
       let answers = [];
       question.answers.forEach(answer =>
         answers.push(
-          <Card key={answer.by.id + answer.createdAt} style={{ margin: "5px" }}>
+          <Card key={answer._id} style={{ margin: "5px" }}>
             <Card.Body>
               {answer.content}
               <br />
@@ -29,9 +36,30 @@ class QuestionSection extends Component {
                   Submitted <TimeAgo date={answer.createdAt} /> by{" "}
                   {`${answer.by.name} - ${answer.by.id}`}
                 </small>
-                <Button variant="outline-success" size="sm">
-                  Helpful
-                </Button>
+                {isStudent && (
+                  <ToggleButtonGroup
+                    type="checkbox"
+                    onChange={value => {
+                      axiosPOST("/api/votes/up", {
+                        reply: answer._id
+                      }).then(() => {
+                        let votes = this.state.votes.Reply;
+                        if (votes[answer._id]) {
+                          delete votes[answer._id];
+                        } else {
+                          votes[answer._id] = "up";
+                        }
+                        this.setState({ "votes.Reply": votes });
+                      });
+                    }}
+                    defaultValue={this.state.votes.Reply[answer._id] ? [1] : []}
+                  >
+                    <ToggleButton value={1} variant="outline-success" size="sm">
+                      {this.state.votes.Reply[answer._id] ? "Marked" : "Mark"}{" "}
+                      as helpful
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                )}
               </div>
             </Card.Body>
           </Card>
@@ -54,13 +82,34 @@ class QuestionSection extends Component {
                     ? "Anonymous"
                     : `${ques.student.name} - ${ques.student.id}`}
                 </small>
-                <Button
-                  variant="outline-secondary"
-                  size="sm"
-                  style={{ marginRight: "5px" }}
-                >
-                  I also had this question
-                </Button>
+                {isStudent && (
+                  <ToggleButtonGroup
+                    type="checkbox"
+                    onChange={value => {
+                      axiosPOST("/api/votes/up", { record: ques._id }).then(
+                        () => {
+                          let votes = this.state.votes.Record;
+                          if (votes[ques._id]) {
+                            delete votes[ques._id];
+                          } else {
+                            votes[ques._id] = "up";
+                          }
+                          this.setState({ "votes.Record": votes });
+                        }
+                      );
+                    }}
+                    defaultValue={this.state.votes.Record[ques._id] ? [1] : []}
+                  >
+                    <ToggleButton
+                      value={1}
+                      variant="outline-secondary"
+                      size="sm"
+                      style={{ marginRight: "5px" }}
+                    >
+                      I also had this question
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                )}
               </div>
               {genAnswers(ques)}
             </Card.Body>
