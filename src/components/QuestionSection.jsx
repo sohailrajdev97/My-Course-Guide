9babeac41a8c934e2ad6f0381bc666eaed1b1bd6
iamkroot <kroot.patel@gmail.com>
2019-04-07 08:19:49 +0530

    questions-sec: Add helpful and same question buttons

--- src/components/QuestionSection.jsx
+++ src/components/QuestionSection.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
-import SeeAll from "./SeeAll";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 import TimeAgo from "react-timeago";
+import SeeAll from "./SeeAll";
 
 class QuestionSection extends Component {
   constructor(props) {
@@ -19,36 +20,50 @@ class QuestionSection extends Component {
       let answers = [];
       question.answers.forEach(answer =>
         answers.push(
-          <Card key={answer}>
+          <Card key={answer.by.id + answer.createdAt} style={{ margin: "5px" }}>
             <Card.Body>
               {answer.content}
               <br />
-              <small>
-                Submitted <TimeAgo date={answer.createdAt} /> by{" "}
-                {`${answer.by.name} - ${answer.by.id}`}
-              </small>
+              <div className="d-flex justify-content-between">
+                <small className="text-muted">
+                  Submitted <TimeAgo date={answer.createdAt} /> by{" "}
+                  {`${answer.by.name} - ${answer.by.id}`}
+                </small>
+                <Button variant="outline-success" size="sm">
+                  Helpful
+                </Button>
+              </div>
             </Card.Body>
           </Card>
         )
       );
 
-      return <SeeAll items={answers} count={1} name="departments" />;
+      return <SeeAll items={answers} count={1} name="answers" />;
     };
     return (
-      <Container id="questions">
+      <Container>
         {this.state.questions.map(ques => (
-          <Card>
+          <Card key={ques._id}>
             <Card.Body>
               {ques.content}
               <br />
-              <small>
-                Submitted <TimeAgo date={ques.createdAt} /> by{" "}
-                {ques.isAnonymous
-                  ? "Anonymous"
-                  : `${ques.student.name} - ${ques.student.id}`}
-              </small>
+              <div className="d-flex justify-content-between">
+                <small className="text-muted">
+                  Submitted <TimeAgo date={ques.createdAt} /> by{" "}
+                  {ques.isAnonymous
+                    ? "Anonymous"
+                    : `${ques.student.name} - ${ques.student.id}`}
+                </small>
+                <Button
+                  variant="outline-secondary"
+                  size="sm"
+                  style={{ marginRight: "5px" }}
+                >
+                  I also had this question
+                </Button>
+              </div>
+              {genAnswers(ques)}
             </Card.Body>
-            {genAnswers(ques)}
           </Card>
         ))}
       </Container>
