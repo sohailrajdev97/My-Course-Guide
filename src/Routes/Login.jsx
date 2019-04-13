e9cc157c8790411826b0ebdc2a22e23954c7df54
Niral <f20170130@hyderabad.bits-pilani.ac.in>
2019-04-13 21:12:30 +0530

    ui: add background images, change font and content of login page

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -3,7 +3,10 @@ import { Redirect } from "react-router";
 import { GoogleLogin } from "react-google-login";
 import { getToken, checkToken } from "../utils/jwt";
 
-import Jumbotron from "react-bootstrap/Jumbotron";
+import LazyHero from "react-lazy-hero";
+import logo from "../assets/logo.png";
+import back1 from "../assets/back1.jpg";
+import back2 from "../assets/back2.jpg";
 
 class Login extends Component {
   constructor(props) {
@@ -36,24 +39,81 @@ class Login extends Component {
       return <Redirect to="/" />;
     } else {
       return (
-        <div className="home">
-          <Jumbotron>
-            <h1>My Course Guide</h1>
-            <p>Login to access course material, reviews and lots more!</p>
-            <p>
-              <GoogleLogin
-                clientId="780021042626-li4g2v5l3pr2s4kdmssdsdtcuc0v2e6m.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={googleSuccess}
-                onFailure={googleFailure}
-              />
-            </p>
-            <font color="red">
-              {this.state.failed
-                ? "Account not found. Make sure you are using your BITS mail to login"
-                : ""}
-            </font>
-          </Jumbotron>
+        <div style={{ fontFamily: "'Josefin Sans', sans-serif" }}>
+          <LazyHero
+            color="#000000"
+            opacity={0.6}
+            imageSrc={back1}
+            minHeight="100vh"
+            parallaxOffset="100"
+            isCentered="false"
+          >
+            <div
+              className="home"
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%,-50%)",
+                textAlign: "center",
+                color: "white"
+              }}
+            >
+              <img src={logo} height="100vh" alt="My Course Guide" />
+              <br />
+              <br />
+              <h2>Helping you make better choices</h2>
+              <br />
+
+              <h5
+                style={{
+                  margin: "10px"
+                }}
+              >
+                Let's Get Started
+              </h5>
+              <p>
+                <GoogleLogin
+                  clientId="780021042626-li4g2v5l3pr2s4kdmssdsdtcuc0v2e6m.apps.googleusercontent.com"
+                  onSuccess={googleSuccess}
+                  onFailure={googleFailure}
+                  theme="dark"
+                  icon={false}
+                  buttonText="Login with BITSMail"
+                />
+              </p>
+              <font color="red">
+                {this.state.failed
+                  ? "Account not found. Make sure you are using your BITS mail to login"
+                  : ""}
+              </font>
+            </div>
+          </LazyHero>
+          <LazyHero
+            minHeight="75vh"
+            opacity={0.8}
+            imageSrc={back2}
+            style={{ textAlign: "center", color: "Black" }}
+          >
+            <h2>
+              <b>
+                <u>about us</u>
+              </b>
+            </h2>
+            <h4 style={{ marginLeft: "10%", marginRight: "10%" }}>
+              We here at My-Course-Guide aim to provide a platform for students
+              as well as professors to give and receive constructive feedbacks
+              about any course(s) they have taken or are currently taken
+            </h4>
+            <div
+              style={{
+                color: "rgb(66, 133, 244)"
+              }}
+            >
+              <br />
+              Made with love from India ❤
+            </div>
+          </LazyHero>
         </div>
       );
     }
